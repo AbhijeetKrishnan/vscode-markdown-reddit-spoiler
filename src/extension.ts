9af0a2d9ab31874bcfef2cca5f4df283d98e9a7c
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2021-04-12 17:37:56 -0700

    Add styling for spoilers

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,9 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
   return {
     extendMarkdownIt(md: any) {
-      return md.use(require('markdown-it-reddit-spoiler').spoiler)
+      return md
+        .use(require('markdown-it-reddit-spoiler').spoiler)
+        .use(require('markdown-it-reddit-spoiler').blockquote)
     }
   };
 }
